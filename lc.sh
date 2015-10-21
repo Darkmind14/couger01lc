49586c68d4bb0ee491f61d13773de97789e358d2
Darkmind14 <eric.coughlin2014@gmail.com>
2015-10-21 11:53:56 -0500

    Changed lc.sh

--- lc.sh
+++ lc.sh
@@ -1,17 +1,19 @@
-#!usr/bin/bash
-lc ()
-{ # function call for cScripts functions
-  c_lowercase $1 
+#!/bin/bash
+  while IFS= read -r -n1 char
+  do 
+     echo "$char"
+  done < test.txt
  
-}
 
 
 
-if [!-e $1 !-e $2]
+
+if [ ! -e $1 ! -e $2 ];
   then
     echo "Error: correct usage requires format"
     echo "lc file1 file2"
     echo "where file1 is file that is to be lower cased and file2 is the name"
     echo "of the resulting file."
-    exit(2)
+    exit 2
 fi
+
