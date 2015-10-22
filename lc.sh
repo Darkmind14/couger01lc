5b58fa5ad60b9bb7945eb855fb5ce5fbb6272c92
Eric Thomas Coughlin <couger01@8271.local>
2015-10-22 14:12:41 -0500

    Finished lc.sh

--- lc.sh
+++ lc.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
-  while IFS= read -r -n1 char
-  do 
-     echo "$char"
-  done < test.txt
- 
+
+  if [ "$#" -ne 2  ];
+    then
+      echo "Error: correct usage requires format"
+      echo "lc.sh file1 file2"
+      echo "where file1 is file that is to be lower cased and file2 is the name"
+      echo "of the resulting file."
+      exit 2
+  fi
 
 
-
-
-if [ ! -e $1 ! -e $2 ];
-  then
-    echo "Error: correct usage requires format"
-    echo "lc file1 file2"
-    echo "where file1 is file that is to be lower cased and file2 is the name"
-    echo "of the resulting file."
-    exit 2
-fi
-
+  ./c_lowercase < $1 > $2
